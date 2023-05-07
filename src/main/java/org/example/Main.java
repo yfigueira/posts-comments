f82f35d61b9f0776c268f1f6cc29b288e10e0415
Yohann Figueira <figueirapy@gmail.com>
2023-05-07 13:24:13 +0200

    Configured Jetty and added Jackson
    Jetty has been configured:
    -> Added Maven Jetty dependencies;
    -> Added Maven Jetty plugin;
    -> Added Jetty configurations in Main;
    -> Created webapp/WEB-INF driectory with web.xml file.
    Additionally added Jackson dependency.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        WebAppContext webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new WebAppConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration()
+        });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+        Server server = new Server(8080);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
     }
 }
