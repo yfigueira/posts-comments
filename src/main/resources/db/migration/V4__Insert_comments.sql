2447100250aece8f74b9b5fa60ef4d3c8801f3ee
Yohann Figueira <figueirapy@gmail.com>
2023-05-09 11:23:51 +0200

    Inserted comments into 'comments' table

---
+++ src/main/resources/db/migration/V4__Insert_comments.sql
@@ -0,0 +1,13 @@
+-- post #1
+INSERT INTO comments(content, date, post_id) VALUES ('&#128525;', '2021-07-07', 1);
+INSERT INTO comments(content, date, post_id) VALUES ('Have fun!', '2021-07-07', 1);
+INSERT INTO comments(content, date, post_id) VALUES ('Beautiful view &#128522;', '2021-07-08', 1);
+
+-- post #2
+INSERT INTO comments(content, date, post_id) VALUES ('WOW!', '2022-07-25', 2);
+INSERT INTO comments(content, date, post_id) VALUES ('&#128518;', '2022-07-25', 2);
+INSERT INTO comments(content, date, post_id) VALUES ('I want one too!', '2022-07-27', 2);
+
+-- post #3
+INSERT INTO comments(content, date, post_id) VALUES ('&#129312;', '2022-08-20', 3);
+INSERT INTO comments(content, date, post_id) VALUES ('Have a nice trip!', '2022-08-21', 3);
