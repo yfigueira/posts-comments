b76303cb5989f089e86dda79cc688102e6b83d70
Yohann Figueira <figueirapy@gmail.com>
2023-05-09 10:55:06 +0200

    Inserted posts into 'posts' table

---
+++ src/main/resources/db/migration/V2__Insert_posts.sql
@@ -0,0 +1,35 @@
+INSERT INTO posts(
+    url,
+    date,
+    number_of_likes,
+    description
+) VALUES (
+    'https://cdn.pixabay.com/photo/2016/11/22/07/26/indiana-dunes-state-park-1848559_960_720.jpg',
+    '2021-07-07',
+    38,
+    '#beachtime!'
+);
+
+INSERT INTO posts(
+    url,
+    date,
+    number_of_likes,
+    description
+) VALUES (
+    'https://cdn.pixabay.com/photo/2013/11/01/11/13/dolphin-203875_960_720.jpg',
+    '2022-07-25',
+    56,
+    'Made a new friend &#128044;'
+);
+
+INSERT INTO posts(
+    url,
+    date,
+    number_of_likes,
+    description
+) VALUES (
+    'https://cdn.pixabay.com/photo/2016/11/21/17/44/arches-national-park-1846759_960_720.jpg',
+    '2022-08-20',
+    43,
+    'On the road!'
+);
