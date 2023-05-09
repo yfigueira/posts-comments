d78c643a7af321b31fecb35cb88416cb14dd2093
Yohann Figueira <figueirapy@gmail.com>
2023-05-09 12:49:25 +0200

    Modified Post into an entity
    Post is now a jakarta persistence Entity

--- src/main/java/org/example/posts/Post.java
+++ src/main/java/org/example/posts/Post.java
@@ -1,14 +1,31 @@
 package org.example.posts;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "posts")
 class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "url")
     private String url;
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "number_of_likes")
     private Integer numberOfLikes;
+
+    @Column(name = "description")
     private String description;
+
+    Post() { }
 
     Post(Integer id, String url, LocalDate date, Integer numberOfLikes, String description) {
         this.id = id;
---
+++ src/main/java/org/example/posts/PostH2DBRepository.java
@@ -0,0 +1,2 @@
+package org.example.posts;public class PostH2DBRepository {
+}
