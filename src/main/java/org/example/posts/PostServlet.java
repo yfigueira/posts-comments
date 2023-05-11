8b9bd87d33a25f45bfdd2394a04f57046ac5e32d
Yohann Figueira <figueirapy@gmail.com>
2023-05-11 12:00:16 +0200

    Added doPut to PostServlet
    Returns updated post or fallback post with adequate http status:
    -> OK when ok,
    -> NOT_FOUND when id has expected format but does not exist in the repository,
    -> BAD_REQUEST when id has an unexpected format.

--- src/main/java/org/example/posts/PostServlet.java
+++ src/main/java/org/example/posts/PostServlet.java
@@ -1,6 +1,7 @@
 package org.example.posts;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -51,7 +52,33 @@ public class PostServlet extends HttpServlet {
 
             mapper.writeValue(resp.getOutputStream(), responsePost);
         } catch (NumberFormatException e) {
-            logger.warn("Bad Request - Unauthorized Id Format: " + requestedId);
+            logger.warn("Bad Request - Unauthorized Id Format");
+
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            mapper.writeValue(resp.getOutputStream(), PostService.fallbackPost());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestedId = req.getPathInfo().substring(1);
+        logger.info("Update On Post " + requestedId + " Requested At " + LocalDateTime.now());
+
+
+        try {
+            Post postToUpdate = mapper.readValue(req.getInputStream(), Post.class);
+            Post responsePost = service.update(postToUpdate);
+
+            resp.setStatus(
+                    responsePost.equals(PostService.fallbackPost())
+                            ? HttpServletResponse.SC_NOT_FOUND
+                            : HttpServletResponse.SC_OK
+            );
+            resp.setContentType("application/json;charset=UTF-8");
+
+            mapper.writeValue(resp.getOutputStream(), responsePost);
+        } catch (InvalidFormatException e) {
+            logger.warn("Bad Request - Unauthorized Id Format");
 
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             mapper.writeValue(resp.getOutputStream(), PostService.fallbackPost());
