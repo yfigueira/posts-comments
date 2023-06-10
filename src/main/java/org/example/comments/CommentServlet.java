28af326ae1d0dc7c238b77a3587dce03a8806efa
Yohann Figueira <figueirapy@gmail.com>
2023-06-10 21:13:30 +0200

    Refactored CommentServlet
    Removed unnecessary library

--- src/main/java/org/example/comments/CommentServlet.java
+++ src/main/java/org/example/comments/CommentServlet.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @WebServlet(name = "Comment", urlPatterns = {"/api/comments/*"})
